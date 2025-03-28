03a92d139159d77f14435446f4bcaab2ca4ad020
nazeeb1432 <126336212+nazeeb1432@users.noreply.github.com>
2025-03-28 23:17:05 +0600

    Doctors.jsx added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import MyProfile from './pages/MyProfile'
 import MyAppointments from './pages/MyAppointments'
 import Appointment from './pages/Appointment'
 import NavBar from './components/NavBar'
+import Footer from './components/Footer'
 
 
 const App = () => {
@@ -27,6 +28,7 @@ const App = () => {
         <Route path='appointment/:docId' element={<Appointment/>}/>
 
       </Routes>
+      <Footer/>
    </div>
   )
 }
